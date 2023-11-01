f489295cc931faebadf1e5f047c8db29b1196351
Niklas Lutze <niklas.lutze@student.kit.edu>
2023-11-01 22:43:19 +0000

    Remove tailwind

---
+++ frontend/components/Button.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+    color: {
+        validator(value: string) {
+            return ['primary'].includes(value)
+        },
+        default: 'primary'
+    }
+})
+defineEmits(["click"])
+</script>
+
+<template>
+    <button @click="$emit('click')" :class="[`color-${color}`]">
+        <slot />
+    </button>
+</template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -9,13 +9,6 @@ export default defineNuxtConfig({
             SPOTIFY_CLIENT_ID: "df3594cdea0a418bade9006fc2b6db29",
         }
     },
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {}
-        }
-    },
-    css: ['~/assets/css/main.css'],
     devtools: { enabled: true }
 })
 
