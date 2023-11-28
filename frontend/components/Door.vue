abe590305860f1659297984c0d2e12c5ffe1f468
Niklas Lutze <niklas.lutze@student.kit.edu>
2023-11-28 01:29:06 +0000

    Add share view

---
+++ frontend/components/Door.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{ door: number; rows: number; cols: number }>();
+const styleObject = computed(() => ({
+    "grid-column-end": "span " + props.cols,
+    "grid-row-end": "span " + props.rows,
+}));
+</script>
+
+<template>
+    <v-sheet
+        color="grey-lighten-3"
+        rounded
+        class="pa-2 justify-center align-center d-flex"
+        :style="styleObject"
+    >
+        {{ door }}
+    </v-sheet>
+</template>
--- frontend/pages/calendars/[id]/share.vue
+++ frontend/pages/calendars/[id]/share.vue
@@ -1,32 +1,56 @@
 <script lang="ts" setup>
-definePageMeta({ middleware: ['auth'] })
-const route = useRoute()
-const calendarId = route.params.id
+definePageMeta({ middleware: ["auth"] });
+const route = useRoute();
+const calendarId = route.params.id as string;
 const password = computed(() => {
     if ("pwd" in route.query) {
-        return route.query["pwd"]
+        return route.query["pwd"];
     } else {
-        return "---"
+        return "---";
     }
-})
+});
 
-const { playlists } = await usePlaylists()
-const { days, calendar } = await useCalendar(calendarId, password)
-const selectedPlaylist = ref(playlists[0])
-const { tracks } = await usePlaylist(selectedPlaylist)
-const { playTrack } = await usePlayer()
+const { calendar } = useCalendar(calendarId);
 
-const getImageOfDay = (door: number) => {
-    return days.value.find(d => d.day == door.toString())?.url
-}
+const { playTrack } = usePlayer();
+
+const { days } = useDays(calendarId);
 </script>
 
 <template>
-    <v-row dense>
-        {{ password }}
-        <v-col v-for="door in 24" :key="door" cols="2">
-            <v-img :src="getImageOfDay(door)" />
-            <v-btn @click="() => playTrack(selectedPlaylist, door - 1)" icon="mdi-play" size="small" />
-        </v-col>
-    </v-row>
+    <div class="calendar">
+        <Door :door="3" :rows="3" :cols="1" />
+        <Door :door="11" :rows="2" :cols="2" />
+        <Door :door="4" :rows="2" :cols="1" />
+        <Door :door="5" :rows="1" :cols="2" />
+        <Door :door="8" :rows="3" :cols="1" />
+        <Door :door="10" :rows="1" :cols="1" />
+        <Door :door="2" :rows="2" :cols="1" />
+        <Door :door="12" :rows="1" :cols="2" />
+        <Door :door="23" :rows="1" :cols="1" />
+        <Door :door="15" :rows="1" :cols="1" />
+        <Door :door="1" :rows="3" :cols="2" />
+        <Door :door="13" :rows="1" :cols="2" />
+        <Door :door="14" :rows="1" :cols="1" />
+        <Door :door="7" :rows="2" :cols="2" />
+        <Door :door="9" :rows="1" :cols="1" />
+        <Door :door="16" :rows="2" :cols="1" />
+        <Door :door="24" :rows="2" :cols="3" />
+        <Door :door="17" :rows="1" :cols="2" />
+        <Door :door="6" :rows="1" :cols="1" />
+        <Door :door="19" :rows="1" :cols="1" />
+        <Door :door="20" :rows="3" :cols="2" />
+        <Door :door="21" :rows="2" :cols="1" />
+        <Door :door="18" :rows="2" :cols="1" />
+        <Door :door="22" :rows="1" :cols="3" />
+    </div>
 </template>
+
+<style>
+.calendar {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(9, 1fr);
+    gap: 8px;
+}
+</style>
